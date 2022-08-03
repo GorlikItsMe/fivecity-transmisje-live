6a495a0ec73e99ebdf0474ff2a81b313f2150457
GorlikItsMe <33355238+GorlikItsMe@users.noreply.github.com>
2022-08-03 10:59:09 +0200

    GoogleAnalytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,15 +1,17 @@
 import type { AppProps } from "next/app";
 import Script from "next/script";
 import "../styles/globals.css";
-import '@fortawesome/fontawesome-free/css/all.css';
+import "@fortawesome/fontawesome-free/css/all.css";
+
+const GA_ID = process.env.NEXT_PUBLIC_GA_ID ?? "";
 
 const App = ({ Component, pageProps }: AppProps) => {
   return (
     <>
       {/* Global Site Tag (gtag.js) - Google Analytics */}
-      {/* <Script
+      <Script
         strategy="afterInteractive"
-        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_ID}`}
+        src={`https://www.googletagmanager.com/gtag/js?id=${GA_ID}`}
       />
       <Script
         id="gtag-init"
@@ -19,12 +21,12 @@ const App = ({ Component, pageProps }: AppProps) => {
             window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
             gtag('js', new Date());
-            gtag('config', '${process.env.NEXT_PUBLIC_GA_ID}', {
+            gtag('config', '${GA_ID}', {
               page_path: window.location.pathname,
             });
           `,
         }}
-      /> */}
+      />
       <Component {...pageProps} />
     </>
   );
