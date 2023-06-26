0788e227b13c5d166db78a1a529c37ef8d057bf7
GorlikItsMe <33355238+GorlikItsMe@users.noreply.github.com>
2023-06-26 21:16:56 +0200

    Debug na prodzie

--- pages/api/v1/avatar.ts
+++ pages/api/v1/avatar.ts
@@ -4,10 +4,13 @@ import { readFileSync } from "fs";
 import Cors from "cors";
 import { runMiddleware } from "../../../lib/runMiddleware";
 import twitchApi from "../../../lib/twitchApi";
+import { createLogger } from "../../../lib/logger";
 
 const cors = Cors({
   methods: ["GET", "HEAD"],
 });
+
+const logger = createLogger("/api/v1/avatar");
 
 async function getIconByUrl(req: NextApiRequest, twitchImageUrl: string) {
   const url = new URL(`http://localhost:3000/_next/image?url=&w=64&q=75`);
@@ -16,6 +19,7 @@ async function getIconByUrl(req: NextApiRequest, twitchImageUrl: string) {
     ? `${req.headers["x-forwarded-proto"]}:`
     : "http:";
   url.host = (req.headers["x-forwarded-host"] as string) ?? "localhost:3000";
+  logger.info(url.href);
 
   const r = await fetch(url.href);
   const buf = await r.arrayBuffer();
@@ -29,10 +33,11 @@ export default async function handler(
   await runMiddleware(req, res, cors);
 
   if ("displayName" in req.query === false) {
-    res.status(400).send("");
+    res.status(400).send("missing displayName");
     return;
   }
   const displayName = `${req.query["displayName"]}`;
+  logger.info(`displayName: ${displayName}`);
 
   const path = join(process.cwd(), "data", "twitchCachedUsersData.json");
   try {
@@ -69,6 +74,7 @@ export default async function handler(
     res.status(404).send("Not found");
     return;
   } catch (error) {
+    logger.error(error);
     res.status(404).send("");
   }
 }
